4fff75014be70473dcf80856271e50b02c159a72
talia <yousfkhaled202020@gmail.com>
2026-08-05 01:27:28 +0300

    feat : first setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000 ;
+
+app.get("/" , (req,res) => {
+    res.send("hello world");
+});
+
+app.listen(PORT , () => {
+    console.log("server running");
+})

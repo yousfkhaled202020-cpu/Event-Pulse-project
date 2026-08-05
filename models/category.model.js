30806a7decd2154c9a2ae7e2fb2dbbe92a1846ce
talia <yousfkhaled202020@gmail.com>
2026-08-05 16:12:10 +0300

    feat: add category schema

---
+++ models/category.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        minlength: 3,
+        maxlength: 100,
+        trim: true,
+        unique: true,
+        default:"",
+        required: [true, "Name is required"]
+    },
+    description: {
+        type: String,
+        default:"",
+    },
+    slug: {
+        type: String,
+        required: [true, "Please Enter the slug of the Category"],
+        unique: true,
+        trim: true,
+        default:"",
+    }
+},
+    {
+        timestamps: true
+    });
+
+module.exports = mongoose.model('Category', categorySchema);

288c0a2be7c9ff5d6aab9cffccf8dda76e50e782
talia <yousfkhaled202020@gmail.com>
2026-08-20 23:04:03 +0300

    feat:sample events and admin data

--- data/categoriesData.js
+++ data/categoriesData.js
@@ -1,25 +1,20 @@
 const techEvents = {
     name:"Tech Events",
-    description:"Explore the latest technologies, innovations, and ideas shaping the future.",
-    slug:"tech"
+    description:"Explore the latest technologies, innovations, and ideas shaping the future."
 }
 const sportsEvents = {
     name:"Sports Events",
-    description:"Competitive events where athletes and teams challenge themselves, follow the rules, and compete for victory.",
-    slug:"sports"
+    description:"Competitive events where athletes and teams challenge themselves, follow the rules, and compete for victory."
 }
 const creativeEvents = {
     name:"Creative Events",
-    description:"Discover events celebrating arts, technology, content creation, and other forms of creativity.",
-    slug:"creative"
+    description:"Discover events celebrating arts, technology, content creation, and other forms of creativity."
 }
 const educationalEvents = {
     name:"Educational Events",
-    description:"Events designed to share knowledge, exchange ideas, and bring communities together to learn.",
-    slug:"educational"
+    description:"Events designed to share knowledge, exchange ideas, and bring communities together to learn."
 }
 const healthEvents = {
     name:"Healthcare Events",
-    description:"Events that bring healthcare professionals and innovators together to share knowledge and explore new technologies.",
-    slug:"health"
+    description:"Events that bring healthcare professionals and innovators together to share knowledge and explore new technologies."
 }

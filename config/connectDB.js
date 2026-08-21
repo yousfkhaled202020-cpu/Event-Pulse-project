c648b7ce44daa9b64b7041a771cf83d47bbaef8f
talia <yousfkhaled202020@gmail.com>
2026-08-21 03:31:08 +0300

    feat: add seeder function in db config

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
+const dataSeeder = require("../seeder/seed");
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
+        await dataSeeder();
         console.log("Database is connected")
     }catch (error){
         console.log("Database connection failed" , error.message);

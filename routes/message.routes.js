7a0f3f961ed023ff4aeb4d4a43f9254ef999b993
talia <yousfkhaled202020@gmail.com>
2026-08-26 18:16:16 +0300

    feat:add socket.io and message routes

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/message.controller.js");
+const { requireAuth } = require("../middleware/requireAuth.js");
+const { requireRole } = require("../middleware/requireRole.js");
 
-// router.get("/" , controller.getAllMessages);
-// router.post("/" , controller.addNewMessage);
-// router.delete("/:id" , controller.cancelMessage);
+router.get("/:eventId" , controller.getHistory);
+router.post("/" , requireAuth , requireRole('admin') ,controller.sendMsg);
 
 module.exports = router;

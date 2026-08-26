27edfded7b6dd675a5818f592db4b32b4280c6d7
talia <yousfkhaled202020@gmail.com>
2026-08-26 18:59:50 +0300

    feat:add socket handler

--- socket/socketHandler.js
+++ socket/socketHandler.js
@@ -1,3 +1,4 @@
+const {sendMsg,getHistory} = require("../controllers/message.controller");
 const socketHandler = (io) =>{
     io.on("connection",async(socket) =>{
         console.log("User connected" , socket.id);
@@ -5,6 +6,14 @@ const socketHandler = (io) =>{
             if (!eventId) return;    
             socket.join(eventId);
             console.log(`User ${socket.id} joined room: ${eventId}`);
+        });
+        socket.on("send-message", async (sender, eventId, content) => {
+            const newMessage = await sendMsg(sender, eventId, content);
+            io.to(eventId).emit("new-message", newMessage);
+        });
+        socket.on("get-history", async (eventId) => {
+            const history = await getHistory(eventId);
+            socket.emit("message-history", history);
         });
         socket.on("leave-event", (eventId) => {
             if (!eventId) return;
